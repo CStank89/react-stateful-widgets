03352845d34084bdc181dc10bf677a9c8ec9fe7c
CStank89 <C.Stank89@gmail.com>
2020-09-07 16:43:04 -0400

    working on squares the other 4 went well now im getting really lost

--- src/components/Moods.js
+++ src/components/Moods.js
@@ -5,7 +5,8 @@ Watch this short video:
 https://tk-assets.lambdaschool.com/fe491c4f-395b-425c-b62e-3fbebf3cbb3f_moods-clip.gif
 
 How many slices of state do you think are necessary to act as "sources of truth" for all
-the things that change in this widget? Give it some thought before continuing reading!
+the things that change in this widget? Give it some thought before continuing reading!\
+ONE!
 
 Some might say 3 different states: one per mood (happy, sad, and uncertain).
 But a single slice of state is more than enough to keep track of all these moods!
@@ -28,7 +29,7 @@ STEPS 4, 5, 6:
   Inside these click handlers set the correct mood, using 'setMood' and the variables below the imports.
 */
 
-import React from 'react'; /* STEP 0 */
+import React, { useState } from 'react'; /* STEP 0 */
 
 const initialMood = 'Not sure how I feel';
 const happyMood = 'Quite happy!';
@@ -36,27 +37,31 @@ const sadMood = 'Rather sad';
 
 export default function Moods() {
   /* STEP 1 */
+  const [mood, setMood] = useState(initialMood)
 
   const makeHappy = () => {
     /* STEP 4 */
+    setMood (happyMood)
   };
   const makeSad = () => {
     /* STEP 5 */
+    setMood (sadMood)
   };
   const reset = () => {
     /* STEP 6 */
+    setMood (initialMood)
   };
 
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'crimson', /* STEP 2 */
+    color: mood === happyMood ? 'crimson' : 'royalblue'
   };
 
   return (
     <div className='widget-moods container'>
       <h2>Moods</h2>
-      <div id='mood' style={style}>Not sure how I feel</div> {/* STEP 3 */}
+      <div id='mood' style={style}>{mood}</div> {/* STEP 3 */}
       <div>
         <button id='makeHappy' onClick={makeHappy}>Make Happy</button>
         <button id='makeSad' onClick={makeSad}>Make Sad</button>
